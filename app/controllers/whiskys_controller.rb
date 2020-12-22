545a17adff48df5f6dcbec15d2018f35adb8171f
Lukasz <lukaszkkoziel@gmail.com>
2020-12-22 22:14:32 +0900

    show

--- app/controllers/whiskys_controller.rb
+++ app/controllers/whiskys_controller.rb
@@ -2,4 +2,7 @@ class WhiskysController < ApplicationController
   def index
     @whiskys = Whisky.all
   end
+  def show
+    @whiskys = Whisky.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "whiskys#index"
+  resources :articles
   get "whiskys/", to: "whiskys#index"
+  get "/whiskys/:id", to: "whiskys#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
